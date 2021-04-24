3dbdca2f7a3bdebbf44e4c956248c7249a8cc78d
HiSandyLee <76956097+HiSandyLee@users.noreply.github.com>
2021-04-24 18:56:37 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+// require packages used in the project
 const express = require('express')
-const app = express()
+// require handlebars in the project
 const exphbs = require('express-handlebars')
 const restaurantList = require('./restaurant.json')
+const app = express()
 const port = 3000
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
@@ -10,24 +12,29 @@ app.set('view engine', 'handlebars')
 // setting static files
 app.use(express.static('public'))
 
+// routes setting
+//home page
 app.get('/', (req, res) => {
   res.render('index', { restaurants: restaurantList.results })
 })
 
-app.get('/search', (req, res) => {
-  const keyword = req.query.keyword
-  const restaurants = restaurantList.results.filter(restaurant => {
-    return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
-  })
-
-  res.render('index', { restaurants: restaurants, keyword: keyword })
-})
-
+//show page
 app.get('/restaurant/:restaurant_id', (req, res) => {
   const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
   res.render('show', { restaurant: restaurant })
 })
 
+//search bar
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const restaurants = restaurantList.results.filter(restaurant => {
+    return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.toLowerCase().includes(keyword.toLowerCase())
+  })
+
+  res.render('index', { restaurants, keyword })
+})
+
+// start and listen on the Express server
 app.listen(port, () => {
   console.log(`Express is listening on http://localhost:${port}`)
 })
